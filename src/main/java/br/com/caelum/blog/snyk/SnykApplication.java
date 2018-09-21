cbc1e81dd2b3dc15874f1d56383d967f64d7367f
Rodrigo Caneppele <r.caneppele@gmail.com>
2018-09-21 16:51:47 -0300

    init project

---
+++ src/main/java/br/com/caelum/blog/snyk/SnykApplication.java
@@ -0,0 +1,12 @@
+package br.com.caelum.blog.snyk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnykApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnykApplication.class, args);
+	}
+}
